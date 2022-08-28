bf6e098991adc63063b52b8cac714dbef6eb0386
PingHuskar <ping47024@icloud.com>
2022-08-29 00:33:59 +0700

    add show recent enroll courses using regex

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 console.log(data.length)
+const target = "_blank"
 document.getElementById("courses").innerHTML = ""
 for (var i = 0; i<data.length;i++) {
-//     document.getElementById("courses").innerHTML +=  `<figure class="snip1104">
-//     <img src="${data[i][2]}" alt="${data[i][0]}" />
-// <figcaption>
-//   <p>${data[i][0]}<span>${data[i][1]}</span></p>
-// </figcaption>
-// <a href="#"></a>
-// </figure>`
-document.getElementById("courses").innerHTML +=
-`<figure class="snip1579"><img src="${data[i][2]}" alt="${data[i][0]}"/>
-<figcaption>
-  <h4>${data[i][0]}</h4>
-  <h5>${data[i][1]}</h5>
-  <blockquote>
-    <p>${data[i][3]} ${data[i][4]}</p>
-  </blockquote>
-</figcaption><a href="${data[i][5]}"></a>
-</figure>`
+  document.getElementById("courses").innerHTML +=
+  `<figure class="snip1579"><img src="${data[i][2]}" alt="${data[i][0]}"/>
+  <figcaption>
+    <h4>${data[i][0]}</h4>
+    <h5>${data[i][1]}</h5>
+    <blockquote>
+      <p>${data[i][3]} ${data[i][4]}</p>
+    </blockquote>
+  </figcaption><a href="${data[i][5]}" target=${target}></a>
+  </figure>`
 }
---
+++ script2.js
@@ -0,0 +1,26 @@
+const target = "_blank"
+const imagesRegex = /(?<=srcset="https:\/\/img-c\.udemycdn\.com\/course\/240x135\/).+?(?=\.jpg)/g;
+const courseIdNameRegex = /(?<=course_id=\d+">).+?(?=<)/g
+const instructorRegex = /(?<=instructors">).+?(?=<)/g
+
+const imagesSrc = [...data.matchAll(imagesRegex)];
+const courseIdName = [...data.matchAll(courseIdNameRegex)];
+const instructor = [...data.matchAll(instructorRegex)];
+var datalist = []
+for (var i = 0; i<imagesSrc.length; i++) {
+  datalist.push([imagesSrc[i][0],courseIdName[i][0],/^\d+/.exec(imagesSrc[i][0])[0],instructor[i][0]])
+}
+console.log(datalist)
+console.log(datalist.length)
+const searchParam = new URLSearchParams(location.search)
+const limit = searchParam.get('limit') || datalist.length
+document.getElementById("courses").innerHTML = ""
+for (var i = 0; i< limit;i++) {
+document.getElementById("courses").innerHTML +=
+`<figure class="snip1579"><img src="https://img-c.udemycdn.com/course/240x135/${datalist[i][0]}.jpg" alt="${datalist[i][1]}"/>
+<figcaption>
+  <h4>${datalist[i][1]}</h4>
+  <h5>${datalist[i][3]}</h5>
+</figcaption><a href="https://www.udemy.com/course-dashboard-redirect/?course_id=${datalist[i][2]}" target=${target}></a>
+</figure>`
+}
