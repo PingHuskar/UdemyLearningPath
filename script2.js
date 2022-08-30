bb10f8f1d39e7e92daf086d4f9625493690b5980
PingHuskar <ping47024@icloud.com>
2022-08-30 22:34:50 +0700

    add filter function

--- script2.js
+++ script2.js
@@ -6,13 +6,15 @@ const instructorRegex = /(?<=instructors">).+?(?=<)/g
 const imagesSrc = [...data.matchAll(imagesRegex)];
 const courseIdName = [...data.matchAll(courseIdNameRegex)];
 const instructor = [...data.matchAll(instructorRegex)];
-console.log(imagesSrc.length,courseIdName.length,instructor.length)
-// alert(`${imagesSrc.length},${courseIdName.length},${instructor.length}`)
+
 var datalist = []
+
 for (var i = 0; i<imagesSrc.length; i++) {
   datalist.push([imagesSrc[i][0],courseIdName[i][0],/^\d+/.exec(imagesSrc[i][0])[0],instructor[i][0]])
 }
 console.log(datalist)
+console.log(datalist.length)
+document.getElementById('countcourses').innerText = datalist.length
 const searchParam = new URLSearchParams(location.search)
 const limit = searchParam.get('limit') || datalist.length
 document.getElementById("courses").innerHTML = ""
@@ -25,3 +27,30 @@ document.getElementById("courses").innerHTML +=
 </figcaption><a href="https://www.udemy.com/course-dashboard-redirect/?course_id=${datalist[i][2]}" target=${target}></a>
 </figure>`
 }
+
+const b = document.getElementsByClassName('snip1579')
+
+const filterButton = document.getElementById('filter')
+const Filter = (e) => {
+  e.preventDefault()
+  const filterValue = document.getElementById('searchFilter').value
+  console.log(filterValue)
+  var c = 0
+  for (var i = 0; i < b.length; i++) {
+    // console.log(b[i].innerText)
+    if (b[i].innerText.toLowerCase().includes(filterValue.toLowerCase())) {
+      b[i].classList.add('show')
+      b[i].classList.remove('hide')
+      c += 1
+    }
+    else {
+      b[i].classList.remove('show')
+      b[i].classList.add('hide')
+    }
+  }
+  document.getElementById('current').innerText = c
+}
+
+document.getElementById('searchFilter').addEventListener('keyup',Filter)
+
+document.getElementById('latest').href = `https://www.udemy.com/user${location.pathname}`
