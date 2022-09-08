3c5451ef3267c8fabd1d41d8c9cacbed07a1712b
PingHuskar <ping47024@icloud.com>
2022-09-08 23:46:03 +0700

    fix jquery (autocomplete) + add animation + add courseId on search

--- js/animation.js
+++ js/animation.js
@@ -4,3 +4,25 @@ TweenMax.staggerFrom('.snip1579',0.7,{
     scale: 0.2,
     opacity: 0
 }, 0.1)
+
+$('.snip1579').hover(function() {
+    TweenMax.to($(this).find('img'),0.1,{
+        autoAlpha:1,ease:Linear.easeNone
+    })
+    TweenMax.to($(this).find('figcaption h4'),0.2,{
+        x:-200,opacity:0,ease:Linear.easeNone
+    })
+    TweenMax.to($(this).find('figcaption h5'),0.2,{
+        x:200,opacity:0,ease:Linear.easeNone
+    })
+}, function() {
+    TweenMax.to($(this).find('figcaption h5'),0.2,{
+        x:0,opacity:1,ease:Back.easeOut
+    })
+    TweenMax.to($(this).find('figcaption h4'),0.2,{
+        x:0,opacity:1,ease:Back.easeOut
+    })
+    TweenMax.to($(this).find('img'),0.1,{
+        autoAlpha:0.3,ease:Back.easeOut
+    })
+})
