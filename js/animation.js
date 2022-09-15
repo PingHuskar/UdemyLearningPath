962137c33cd9e92bc001d7c810ed409f826ddd35
PingHuskar <ping47024@icloud.com>
2022-09-15 23:13:54 +0700

    fix style

--- js/animation.js
+++ js/animation.js
@@ -6,9 +6,9 @@ TweenMax.staggerFrom('.snip1579',2,{
 }, 0.05)
 
 $('.snip1579').hover(function() {
-    TweenMax.to($(this).find('img'),0.1,{
-        autoAlpha:1,ease:Linear.easeNone
-    })
+    // TweenMax.to($(this).find('img'),0.1,{
+    //     autoAlpha:1,ease:Linear.easeNone
+    // })
     TweenMax.to($(this).find('figcaption h4'),0.2,{
         x:-200,opacity:0,ease:Linear.easeNone
     })
@@ -22,7 +22,7 @@ $('.snip1579').hover(function() {
     TweenMax.to($(this).find('figcaption h4'),0.2,{
         x:0,opacity:1,ease:Back.easeOut
     })
-    TweenMax.to($(this).find('img'),0.1,{
-        autoAlpha:0.3,ease:Back.easeOut
-    })
+    // TweenMax.to($(this).find('img'),0.1,{
+    //     autoAlpha:0.3,ease:Back.easeOut
+    // })
 })
