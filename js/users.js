6e488f11f8e26cf700bb88354d33ee2dbcec8c3f
PingHuskar <ping47024@icloud.com>
2022-09-17 15:28:59 +0700

    rename

--- js/users.js
+++ js/users.js
@@ -2,5 +2,5 @@ const users = [
     { gh: "pinghuskar", uurl: "chadinchaipornpisuth2", uid: "11714446" },
     { gh: "mayanth", uurl: "anothai-srisangthum", uid: "204011072" },
     { gh: "immortalgky", uurl: "jirakit", uid: "24370922" },
-    { gh: "ChunyaAmornleetrakul", uurl: "chunya-2", uid: "101877998" }
+    { gh: "PimLyn", uurl: "chunya-2", uid: "101877998" }
 ]
